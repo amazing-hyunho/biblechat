0dca691dd58bd215ba21c5f807b11b8ecf623461
amazing-hyunho <gusgh6605@gmail.com>
2025-03-16 01:20:02 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-import os
-from dotenv import load_dotenv
-
-# .env 파일 로드
-load_dotenv()
+import streamlit as st
 
 # OpenAI API 키 설정
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
