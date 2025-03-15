5ad6299e94062e871623c3765a85ec7cd0c84d1c
amazing-hyunho <gusgh6605@gmail.com>
2025-03-16 01:12:59 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,5 @@
-from pyngrok import ngrok
 import os
 from dotenv import load_dotenv
-
-
-#PYNGROK_API_KEY = '2rN06yr538OB5MV6zru6pZhszsp_3qpEMf8MCJvstrysKVSDx'
-# ngrok 서비스 인증
-#ngrok.set_auth_token(PYNGROK_API_KEY)
-#ngrok_tunnel = ngrok.connect(addr='5011', proto='http', bind_tls=True)
-#print(' * Tunnel URL:', ngrok_tunnel.public_url)
-
 
 # .env 파일 로드
 load_dotenv()
